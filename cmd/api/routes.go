24cf34ca9eb09b702de640851c36dfae23e368dc
VaudKK <vaudkeith@gmail.com>
2025-05-01 14:55:38 +0300

    feat: securing contributions

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,18 +11,18 @@ func (app *application) routes() http.Handler {
 	subRouter := mx.PathPrefix("/api/v1").Subrouter()
 
 	// contributions
-	subRouter.Handle("/contributions/import",app.requiredAuthenticatedUser(app.upload)).Methods("POST")
-	subRouter.Handle("/contributions", app.requiredAuthenticatedUser(app.addContribution)).Methods("POST")
-	subRouter.Handle("/contributions/search", app.requiredAuthenticatedUser(app.search)).Methods("GET")
-	subRouter.Handle("/contributions/stats",app.requiredAuthenticatedUser(app.getMonthlyStats)).Methods("GET")
-	subRouter.Handle("/contributions/categories/all",app.requiredAuthenticatedUser(app.getCategories)).Methods("GET")
-	subRouter.Handle("/contributions/{id}", app.requiredAuthenticatedUser(app.updateContribution)).Methods("PUT")
+	subRouter.Handle("/contributions/import", app.requiresAuthenticatedUser(app.upload)).Methods("POST")
+	subRouter.Handle("/contributions", app.requiresAuthenticatedUser(app.addContribution)).Methods("POST")
+	subRouter.Handle("/contributions/search", app.requiresAuthenticatedUser(app.search)).Methods("GET")
+	subRouter.Handle("/contributions/stats", app.requiresAuthenticatedUser(app.getMonthlyStats)).Methods("GET")
+	subRouter.Handle("/contributions/categories/all", app.requiresAuthenticatedUser(app.getCategories)).Methods("GET")
+	subRouter.Handle("/contributions/{id}", app.requiresAuthenticatedUser(app.updateContribution)).Methods("PUT")
 
 	// user
 	subRouter.HandleFunc("/auth/signup", app.createUser).Methods("POST")
-	subRouter.HandleFunc("/auth/login",app.issueToken).Methods("POST")
-	subRouter.HandleFunc("/auth/otp/send",app.sendOtp).Methods("GET")
-	subRouter.HandleFunc("/auth/otp/verify",app.verifyOtp).Methods("GET")
+	subRouter.HandleFunc("/auth/login", app.issueToken).Methods("POST")
+	subRouter.HandleFunc("/auth/otp/send", app.sendOtp).Methods("GET")
+	subRouter.HandleFunc("/auth/otp/verify", app.verifyOtp).Methods("GET")
 
 	return app.recoverPanic(app.authenticate(subRouter))
 }
